56ba70a66dca2394408ba53091768f31d73443c2
root <keithohno@gmail.com>
2022-04-30 02:19:57 +0000

    removed json size limit

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import express from "express";
 import router from "./router";
 
 var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use(function (req, res, next) {
   var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
